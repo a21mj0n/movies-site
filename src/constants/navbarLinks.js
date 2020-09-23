de0287af00a42d5e1076d6b66820f6dc75cc86b0
a21mj0n <azimjontoirov@gmail.com>
2020-09-23 22:50:36 +0500

    some navbar links commented

--- src/constants/navbarLinks.js
+++ src/constants/navbarLinks.js
@@ -1,16 +1,16 @@
 export const NAVBAR_LINKS = [
-  {
-    id: 2,
-    name: "movies",
-    url: "movies",
-    icon: "video_library"
-  },
-  {
-    id: 3,
-    name: "celebrities",
-    url: "celebrities",
-    icon: "wc"
-  },
+  // {
+  //   id: 2,
+  //   name: "movies",
+  //   url: "movies",
+  //   icon: "video_library"
+  // },
+  // {
+  //   id: 3,
+  //   name: "celebrities",
+  //   url: "celebrities",
+  //   icon: "wc"
+  // },
   {
     id: 4,
     name: "search",
